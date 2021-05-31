80df698b0b606dca77a5db4156f803dd447ee783
Rocky Das <76746655+rockydasgfx@users.noreply.github.com>
2021-05-31 20:53:58 +0600

    Me birthday

--- asset/app.js
+++ asset/app.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date('may 10, 2021 00:00:00').getTime();
+    const countDate = new Date('july 17, 2021 00:00:00').getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
     
